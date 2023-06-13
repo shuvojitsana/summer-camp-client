71cfc21ec9ae66bcde28a932744be2a1afd735f4
shuvojitsana <shuvojitsanabd2004@gmail.com>
2023-06-13 13:36:47 +0600

    added a push

--- src/pages/PopularClass/PopularClass.jsx
+++ src/pages/PopularClass/PopularClass.jsx
@@ -2,36 +2,36 @@ import { useEffect, useState } from "react";
 import AllClassCard from "../Allclasses/AllClassCard";
 
 
-const PopularClass = () => {
-    const [menu, setMenu] = useState([]);
+// const PopularClass = () => {
+//     const [menu, setMenu] = useState([]);
 
-    useEffect(() => {
-        fetch('allClasses.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular_teacher')
-                setMenu(popularItems);
-            })
-    }, [])
-    return (
+//     useEffect(() => {
+//         fetch('allClasses.json')
+//             .then(res => res.json())
+//             .then(data => {
+//                 const popularItems = data.filter(item => item.category === 'popular_teacher')
+//                 setMenu(popularItems);
+//             })
+//     }, [])
+//     return (
 
-        <div className=" my-10">
+//         <div className=" my-10">
 
-            <div className="text-center">
-                <p className="font-mono text-[#ea4a34]">Start now</p>
-                <h2 className="text-4xl"> Course Categories</h2>
-            </div>
+//             <div className="text-center">
+//                 <p className="font-mono text-[#ea4a34]">Start now</p>
+//                 <h2 className="text-4xl"> Course Categories</h2>
+//             </div>
 
-            <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
-                {
-                    menu.map(item => <AllClassCard
-                        key={item._id}
-                        item={item}
-                    ></AllClassCard>)
-                }
-            </div>
-        </div>
-    );
-};
+//             <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
+//                 {
+//                     menu.map(item => <AllClassCard
+//                         key={item._id}
+//                         item={item}
+//                     ></AllClassCard>)
+//                 }
+//             </div>
+//         </div>
+//     );
+// };
 
 export default PopularClass;
